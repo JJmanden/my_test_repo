18e22e5c4cd959c990e7d5b7fe7be64149eede7e
JJmanden <johannhvsk@hotmail.com>
2025-10-03 17:53:27 +0200

    Første upload af talleg.py

---
+++ talleg.py
@@ -0,0 +1,18 @@
+import random
+
+# Vælg et tilfældigt tal mellem 1 og 100
+hemmeligt_tal = random.randint(1, 100)
+
+print("Gæt et tal mellem 1 og 100!")
+
+while True:
+    # Brugeren gætter
+    gæt = int(input("Dit gæt: "))
+
+    if gæt < hemmeligt_tal:
+        print("Højere!")
+    elif gæt > hemmeligt_tal:
+        print("Lavere!")
+    else:
+        print("Du har gættet rigtigt 🎉")
+        break
